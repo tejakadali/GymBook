5be36f952191dc62eecdb2c9365eb2deb17ed711
kstejas <simhateja.kadali@gmail.com>
2017-05-12 19:12:15 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+      	$('#tcf-slider').tcf_slider({
+        	images: [{
+        		src: "https://unsplash.it/800/600?image=281",
+				caption:"Victor Erixon"
+        	}, {
+        		src: "https://unsplash.it/800/600?image=680"
+				,
+				caption:"Caption 2"
+        	}, {
+        		src: "https://unsplash.it/800/600?image=717",
+				caption:"Caption 3"
+        	}, {
+        		src: "https://unsplash.it/800/600?image=689",
+				caption:"Caption 4"
+        	}, {
+        		src: "https://unsplash.it/800/600?image=690",
+				caption:"Caption 5"
+        	}]
+      	})
+    	})
